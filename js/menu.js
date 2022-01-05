d8a7705eb638fb57ef218d8de61e7bc79487ae74
josueprogram2003 <josuedominguezonline12@gmail.com>
2022-01-04 20:46:43 -0500

    josue

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@ const btnMenu = document.getElementById("btn-menu");
 btnMenu.addEventListener("click", () => {
   const menu = document.getElementById("main-nav");
   const height = menu.scrollHeight;
-  if (menu.classList.contains("ocultar")) {
-    menu.classList.remove("ocultar");
-    menu.style.height = height + "px";
-  } else {
-    menu.classList.add("ocultar");
+  if (menu.classList.contains("desplegar")) {
+    menu.classList.remove("desplegar");
     menu.removeAttribute("style");
+  } else {
+    menu.classList.add("desplegar");
+    menu.style.height = height + "px";
   }
 });
